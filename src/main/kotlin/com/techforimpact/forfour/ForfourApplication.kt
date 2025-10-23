6742d46e24a087d51516cda13cab8834c387ab6c
koreaioi <koreaioi@gachon.ac.kr>
2025-10-23 21:10:40 +0900

    chore: 초기 세팅

---
+++ src/main/kotlin/com/techforimpact/forfour/ForfourApplication.kt
@@ -0,0 +1,11 @@
+package com.techforimpact.forfour
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForfourApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForfourApplication>(*args)
+}
---
+++ src/test/kotlin/com/techforimpact/forfour/ForfourApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.techforimpact.forfour
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForfourApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
