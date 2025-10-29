ea6ae5afee5175d9cfa303247f5970d903f8d41b
koreaioi <koreaioi@gachon.ac.kr>
2025-10-29 18:20:59 +0900

    feat: Auth 관련 상수 추가

---
+++ src/main/kotlin/com/techforimpact/forfour/global/auth/constants/AuthConstants.kt
@@ -0,0 +1,7 @@
+package com.techforimpact.forfour.global.auth.constants
+
+const val AUTH_HEADER = "Authorization"
+const val ACCESS_TOKEN_HEADER = "AccessToken"
+const val AUTH_PREFIX = "Bearer "
+const val ID_CLAIM = "id"
+const val ROLE_CLAIM = "role"
